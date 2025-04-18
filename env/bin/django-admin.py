977b32efcb6a095930f5281d77c114e7f03469c9
dogmatus07 <steve.raharison@gmail.com>
2025-04-18 16:40:12 +0300

    Initial commit before refacto

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/steve/Documents/Dev/oc-lettings/Python-OC-Lettings-FR/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
