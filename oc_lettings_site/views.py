31c6f95d786f5c9d6ff0aba58cc335ce2bf57dea
dogmatus07 <steve.raharison@gmail.com>
2025-04-24 17:06:33 +0300

    Views and Templates refacto done

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -3,19 +3,13 @@ from lettings.models import Letting
 from profiles.models import Profile
 
 
-
-# Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie quam lobortis leo consectetur ullamcorper non id est. Praesent dictum, nulla eget feugiat sagittis, sem mi convallis eros,
-# vitae dapibus nisi lorem dapibus sem. Maecenas pharetra purus ipsum, eget consequat ipsum lobortis quis. Phasellus eleifend ex auctor venenatis tempus.
-# Aliquam vitae erat ac orci placerat luctus. Nullam elementum urna nisi, pellentesque iaculis enim cursus in. Praesent volutpat porttitor magna, non finibus neque cursus id.
 def index(request):
     return render(request, 'index.html')
 
-# Aenean leo magna, vestibulum et tincidunt fermentum, consectetur quis velit. Sed non placerat massa. Integer est nunc, pulvinar a
-# tempor et, bibendum id arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras eget scelerisque
 def lettings_index(request):
     lettings_list = Letting.objects.all()
     context = {'lettings_list': lettings_list}
-    return render(request, 'lettings_index.html', context)
+    return render(request, 'index.html', context)
 
 
 def letting(request, letting_id):
@@ -29,7 +23,7 @@ def letting(request, letting_id):
 def profiles_index(request):
     profiles_list = Profile.objects.all()
     context = {'profiles_list': profiles_list}
-    return render(request, 'profiles_index.html', context)
+    return render(request, 'index.html', context)
 
 def profile(request, username):
     profile = Profile.objects.get(user__username=username)
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Profile
 
-# Create your views here.
+
+def index(request):
+    """
+    View function for the profiles index page.
+    :param request:
+    :return:
+    """
+    profiles_list = Profile.objects.all()
+    context = {'profiles_list': profiles_list}
+    return render(request, 'profiles/index.html', context)
+
+
+def profile(request, username):
+    """
+    View function for a specific profile.
+    :param request:
+    :param username:
+    :return:
+    """
+    profile = get_object_or_404(Profile, user__username=username)
+    context = {
+        'profile': profile,
+        'favorite_city': profile.favorite_city,
+    }
+    return render(request, 'profiles/profile.html', context)
