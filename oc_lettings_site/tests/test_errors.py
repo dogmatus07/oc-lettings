1c99df01d7d411e53057ee3c3c87305ef1cb42fa
dogmatus07 <steve.raharison@gmail.com>
2025-04-29 19:16:26 +0300

    Tests Done

--- oc_lettings_site/tests/test_errors.py
+++ oc_lettings_site/tests/test_errors.py
@@ -1,8 +1,6 @@
-import types
 import pytest
-from django.urls import reverse
-from django.http import HttpResponseServerError
-from django.urls import path, clear_url_caches, set_urlconf
+from django.test import override_settings, RequestFactory
+from django.views import defaults
 
 
 @pytest.mark.django_db
@@ -11,28 +9,46 @@ def test_custom_404_view(client):
     Test the custom 404 error page
     :param client: test client
     """
-    response = client.get('/not-a-page/')
+    response = client.get('not-a-page/')
     assert response.status_code == 404
     assert b'Page not found' in response.content
 
 
 @pytest.mark.django_db
-def test_custom_500_view(client):
-    """
-    Test the custom 500 error page
-    :param client: test client
-    """
-    def error_view(request):
-        raise Exception("Test exception")
-
-    test_urls = types.ModuleType("test_urls")
-    test_urls.urlpatterns = [
-        path('force-500/', error_view)
+@override_settings(
+    DEBUG=False,
+    ROOT_URLCONF='oc_lettings_site.tests.urls_test_500',
+    TEMPLATES=[
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': ['oc_lettings_site/templates'],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
+        },
     ]
+)
+def test_custom_500_view():
+    """
+    Test the custom 500 error page, simulate an exception
+    Call the default Django 500 error view
+    """
 
-    clear_url_caches()
-    set_urlconf(test_urls)
-    response = client.get('/force-500/', follow=False, raise_request_exception=False)
+    factory = RequestFactory()
+    request = factory.get('/force-500/')
+    response = None
+
+    try:
+        from oc_lettings_site.tests.urls_test_500 import error_view
+        error_view(request)
+    except Exception:
+        response = defaults.server_error(request, template_name='500.html')
+
     assert response.status_code == 500
     assert b'Server Error' in response.content
-
