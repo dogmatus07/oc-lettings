a80ad5e6009f6c3d9d8d5e04ac40342c9f15c263
dogmatus07 <steve.raharison@gmail.com>
2025-05-01 18:47:37 +0300

    Logs setup

--- lettings/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class LettingsConfig(AppConfig):
-    """
-    Configuration class for the lettings app.
-    """
-    name = 'lettings'
--- profiles/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProfilesConfig(AppConfig):
-    """
-    Configuration class for the Profiles application.
-    """
-    name = 'profiles'
