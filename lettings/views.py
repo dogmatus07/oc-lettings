2e671f134ef7c73b78cb7120698cbd06cf5bd7cd
dogmatus07 <steve.raharison@gmail.com>
2025-05-10 08:53:07 +0300

    Sentry logs done

--- lettings/views.py
+++ lettings/views.py
@@ -1,5 +1,9 @@
+import logging
+import sentry_sdk
 from django.shortcuts import render, get_object_or_404
 from .models import Letting
+
+logger = logging.getLogger(__name__)
 
 
 def index(request):
@@ -8,9 +12,14 @@ def index(request):
     :param request:
     :return: lettings/index.html
     """
-    lettings_list = Letting.objects.all()
-    context = {'lettings_list': lettings_list}
-    return render(request, 'lettings/index.html', context)
+    try:
+        lettings_list = Letting.objects.all()
+        context = {'lettings_list': lettings_list}
+        return render(request, 'lettings/index.html', context)
+    except Exception as e:
+        logger.error("An error occurred while rendering the index page: %s", e)
+        sentry_sdk.capture_exception(e)
+        return render(request, '500.html', status=500)
 
 
 def letting(request, letting_id):
@@ -20,9 +29,15 @@ def letting(request, letting_id):
     :param letting_id: The ID of the letting to display.
     :return: lettings/letting.html
     """
-    letting = get_object_or_404(Letting, id=letting_id)
-    context = {
-        'title': letting.title,
-        'address': letting.address,
-    }
-    return render(request, 'lettings/letting.html', context)
+    try:
+        letting = get_object_or_404(Letting, id=letting_id)
+        context = {
+            'title': letting.title,
+            'address': letting.address,
+        }
+        return render(request, 'lettings/letting.html', context)
+    except Exception as e:
+        logger.error("Error in rendering the letting detail view,"
+                     "ID: %s, Error: %s", letting_id, e)
+        sentry_sdk.capture_exception(e)
+        return render(request, '500.html', status=500)
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,9 @@
+import logging
+import sentry_sdk
 from django.shortcuts import render, get_object_or_404
 from .models import Profile
+
+logger = logging.getLogger(__name__)
 
 
 def index(request):
@@ -8,9 +12,14 @@ def index(request):
     :param request:
     :return: profiles/index.html
     """
-    profiles_list = Profile.objects.all()
-    context = {'profiles_list': profiles_list}
-    return render(request, 'profiles/index.html', context)
+    try:
+        profiles_list = Profile.objects.all()
+        context = {'profiles_list': profiles_list}
+        return render(request, 'profiles/index.html', context)
+    except Exception as e:
+        logger.error("An error occurred while rendering the index page: %s", e)
+        sentry_sdk.capture_exception(e)
+        return render(request, '500.html', status=500)
 
 
 def profile(request, username):
@@ -20,9 +29,15 @@ def profile(request, username):
     :param username: The username of the profile to display.
     :return: profiles/profile.html
     """
-    profile = get_object_or_404(Profile, user__username=username)
-    context = {
-        'profile': profile,
-        'favorite_city': profile.favorite_city,
-    }
-    return render(request, 'profiles/profile.html', context)
+    try:
+        profile = get_object_or_404(Profile, user__username=username)
+        context = {
+            'profile': profile,
+            'favorite_city': profile.favorite_city,
+        }
+        return render(request, 'profiles/profile.html', context)
+    except Exception as e:
+        logger.error("Error in rendering the profile detail view,"
+                     "Username: %s, Error: %s", username, e)
+        sentry_sdk.capture_exception(e)
+        return render(request, '500.html', status=500)
