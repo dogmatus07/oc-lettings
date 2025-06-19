2ab0596a3c6e48a633f0c47a7311b17667d6c0d7
dogmatus07 <steve.raharison@gmail.com>
2025-06-19 10:11:13 +0300

    Ready for deploy on Render

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # This script is used to run the Django application with Gunicorn
 python manage.py migrate --noinput
-python load_fixtures.py
+for file in fixtures/*.json; do
+    python manage.py loaddata "$file"
+done
 python manage.py collectstatic --noinput
 exec gunicorn oc_lettings_site.wsgi:application --bind 0.0.0.0:8000
