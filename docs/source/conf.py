535d3835b8d5957da465e8388c0f3bed25c8d933
dogmatus07 <steve.raharison@gmail.com>
2025-06-20 15:14:04 +0300

    Read The Docs setup done

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,13 +7,17 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
+import pathlib
 
 # Root to the project directory
-sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'oc_lettings_site.settings'
+REPO_ROOT = pathlib.Path(__file__).resolve().parents[2]
+sys.path.insert(0, str(REPO_ROOT))
+
+# Set Django settings module
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oc_lettings_site.settings")
 
 # Import Django and setup the environment
-import django
+import django  # noqa: E402
 django.setup()
 
 
@@ -28,10 +32,12 @@ release = '1.0'
 
 # Extensions to use
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc.typehints"
 ]
+
 
 # Paths to templates and excluded patterns
 templates_path = ['_templates']
@@ -43,3 +49,17 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# Autodoc default options
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": False,
+    "private-members": False,
+    "special-members": "__str__",
+    "show-inheritance": True,
+}
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+napoleon_include_init_with_doc = True
