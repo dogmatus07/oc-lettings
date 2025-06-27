9e6c3426f697021ea11159c391eb5e3b1f0f0da4
dogmatus07 <steve.raharison@gmail.com>
2025-06-27 13:09:19 +0300

    Ready to deploy for changes

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -49,7 +49,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost 127.0.0.1").split()
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost 127.0.0.1 0.0.0.0").split()
 
 
 # Application definition
